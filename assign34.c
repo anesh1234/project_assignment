35b396118db4818b38846a852a1a6164638db0a4
anesh1234 <anders.minde@gmail.com>
2024-08-29 12:27:35 +0000

    task 33 working

---
+++ assign34.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <openssl/bn.h>
+#define NBITS 256
+
+void printBN(char * msg, BIGNUM * a)
+{
+    // Use BN_bn2hex(a) for hex string
+    // Use BN_bn2dec(a) for decimal string
+    char * number_str = BN_bn2hex(a);
+    printf("%s %s\n", msg, number_str);
+    OPENSSL_free(number_str);
+}
+
+
+int main ()
+{
+	// Setup; the variables needed
+	BN_CTX *ctx = BN_CTX_new();
+
+	BIGNUM *n = BN_new();
+	BN_hex2bn(&n,"DCBFFE3E51F62E09CE7032E2677A78946A849DC4CDDE3A4D0CB81629242FB1A5");
+	
+	BIGNUM *e = BN_new();
+        BN_hex2bn(&e,"010001");
+	
+	BIGNUM *d = BN_new();
+        BN_hex2bn(&d,"74D806F9F3A62BAE331FFE3F0A68AFE35B3D2E4794148AACBC26AA381CD7D30D");
+	
+	char *msg = "I owe you $2000.";
+	char *msg2 = "I owe you $3000.";
+
+	// The RSA formula for decryption is c^d, where c is the cipher text and d is the private key
+	BN_mod_exp(dc, C, d, n, ctx);
+	char *msg2 = "The deciffered cipher text is: ";
+        printBN(msg2, dc);
+
+    return 0;
+
+}
+ 
